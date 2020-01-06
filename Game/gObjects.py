705af759615ec424802ac71f427cbc7bbe7768fc
Highjin <vladonnx@mail.ru>
2020-01-06 23:49:37 +0300

    INIT.GAME

---
+++ Game/gObjects.py
@@ -0,0 +1,13 @@
+import pygame
+import Game
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(Game.GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 5
